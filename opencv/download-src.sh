349a8e58225222911a163cb3e6cdf080e103eaca
caesar <caesar@kekxv.com>
2024-01-09 21:54:30 +0800

    update

--- opencv/download-src.sh
+++ opencv/download-src.sh
@@ -2,6 +2,6 @@
 
 cd `dirname $0`
 rm -rf "opencv-src"
-wget -q -c "https://github.com/opencv/opencv/archive/refs/tags/$1.tar.gz"
+wget -c "https://github.com/opencv/opencv/archive/refs/tags/$1.tar.gz"
 tar -xf "$1.tar.gz"
 mv "opencv-$1" opencv-src
